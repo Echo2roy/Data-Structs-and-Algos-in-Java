05affad710c3c9bbb4e819a30bd3746de176a12c
Roy Esibe <bichiroy50@gmail.com>
2019-01-23 19:38:35 +0100

    Add comments

--- P1_26.java
+++ P1_26.java
@@ -6,9 +6,10 @@ public class P1_26 {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter string:  ");
 
+        // Stores each string in array cell separated by whitespaces
         String[] s = input.nextLine().split(" ");
 
-
+        //iterates through array and displays each string
         for(int t = (s.length-1); t >= 0; t--){
             System.out.print(s[t] + " ");
         }
