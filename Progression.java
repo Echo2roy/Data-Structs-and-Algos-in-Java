9a82a01ef19fd77c5002b032765877991dfaa93d
Roy Esibe <bichiroy50@gmail.com>
2019-01-26 23:41:03 +0100

    Implement progression method

---
+++ Progression.java
@@ -0,0 +1,36 @@
+
+public class Progression {
+
+    //instance variable
+
+    protected long current;
+
+    // Constructs a progression starting from 0
+    public Progression(){this(0);}
+
+    //constructs a progression with a given start value
+
+    public Progression(long start){current = start;}
+
+    //Returns the net value of the progression
+    public long nextValue(){
+        long answer = current;
+        advance(); // this protected call is responsibe for advancing the current value
+        return answer;
+    }
+
+    //Advances the current value to the next value of the progression
+    protected void advance(){
+        current++;
+    }
+
+    //Prints the next n values of the progression, separated by the spaces
+
+    public void printProgression(int n){
+        System.out.print(nextValue());
+        for(int J = 1; J < n; j++)
+            System.out.println(" " + nextValue());
+        System.out.println();
+    }
+
+}
